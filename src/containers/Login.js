40fce7f861699e46c6df64ab83da66ea93c2406e
mijung_kwak <mijung.kwak@bespinglobal.com>
2020-04-01 16:25:06 +0900

    중간저장

---
+++ src/containers/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import "./Login.css";
+import { Auth } from "aws-amplify";
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+//   로그인할때 홈으로 리다이렉션
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    setIsLoading(true);
+    // 로그인할때 화살표 빙글빙글 거리는거
+
+    try {
+      await Auth.signIn(email, password);
+      props.userHasAuthenticated(true);
+      props.history.push("/"); 
+    // ↑ 리다이렉션 가능
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+        >
+            Login
+        </LoaderButton>
+      </form>
+    </div>
+  );
+
+
+
+}
